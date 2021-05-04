ae8bd4d1ef828a2b8b045b2c20cdaa38d6dbb3a8
taaako <ta12ko.sa@gmail.com>
2021-05-05 00:04:46 +0900

    dot install #5 Enum

--- main.ts
+++ main.ts
@@ -16,3 +16,33 @@ x = "hello";
 // 配列
 var results: number[];
 results = [10, 5, 3];
+
+// 列挙型
+// Signal
+
+// enum Signal {
+//     Red = 0,
+//     Blue = 1,
+//     Yellow = 2
+// }
+
+enum Signal {
+    Red, // 0
+    Blue = 3, // 3
+    Yellow // 4
+}
+
+// Signalの拡張 mergeされる
+// 複数の列挙型を宣言した場合、最初のメンバを省略できるのはどちらかのみ
+enum Signal {
+    Green = 5
+}
+
+var result: Signal;
+
+// if (result === Signal.Yellow) {...}
+// if (result === Signal['Yellow']) {...}
+
+// console.log(Signal[2]); // Yellow
+// console.log(Signal[3]); // Blue
+console.log(Signal.Green); // 5
