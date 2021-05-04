d334b2673c08565f6f4c556d9e0b45c624b6ca6c
taaako <ta12ko.sa@gmail.com>
2021-05-05 00:53:15 +0900

    dot install #10 private class

--- main.ts
+++ main.ts
@@ -4,19 +4,41 @@
 // defalt: public (省略可)
 // protected, private
 
+// public
+// class User {
+//     constructor(public name: string) {
+//     }
+//     public sayHi(): void {
+//         console.log("hi! i am " + this.name);
+//     }
+// }
+
+// var tom = new User("Tom");
+// console.log(tom.name);
+// tom.sayHi();
+
+// private
+// クラス内のみで呼び出し可能
+// getter / setter を使用すれば柔軟に活用できる
+// この場合、ECMAScript5を使えと言われてerrorとなるので、コンソールでコマンド「-t ES5」を付け加える
 class User {
-    // public name: string;
-    // constructor(name: string) {
-    //     this.name = name;
-    // }
-    // 上記と同じ、省略版
-    constructor(public name: string) {
+    constructor(private _name: string) {
     }
     public sayHi(): void {
-        console.log("hi! i am " + this.name);
+        console.log("hi! i am " + this._name);
+    }
+    // getter
+    get name() {
+        return this._name;
+    }
+    // setter
+    set name(newValue: string) {
+        this._name = newValue;
     }
 }
 
 var tom = new User("Tom");
+console.log(tom.name);
+tom.name = "TOM";
 console.log(tom.name);
 tom.sayHi();
