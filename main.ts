599bb8d234423d7f69fd9d728394a6402beb0f9d
taaako <ta12ko.sa@gmail.com>
2021-05-05 00:36:18 +0900

    dot install #8 function overload

--- main.ts
+++ main.ts
@@ -37,6 +37,20 @@
 // }
 
 // アロー関数式のさらに簡略版
-var add = (a: number, b: number): number => a + b;
+// var add = (a: number, b: number): number => a + b;
 
-console.log(add(5, 3));
+// 関数のオーバーロード
+function add(a: number, b: number): number; // シグネチャ
+function add(a: string, b: string): string;
+
+function add(a: any, b: any): any {
+    if (typeof a === "string" && typeof b === "string") {
+        return a + " " + b;
+    }
+    return a + b;
+}
+
+console.log(add(5, 3)); // 8
+console.log(add("hello", "world")); // hello world
+
+console.log(add("hello", 3)); // error
