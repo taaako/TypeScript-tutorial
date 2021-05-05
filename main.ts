f8d3ef6704122b5060cdefa18503bdab08052c79
taaako <ta12ko.sa@gmail.com>
2021-05-05 21:37:09 +0900

    dot install #13 interface

--- main.ts
+++ main.ts
@@ -1,92 +1,20 @@
-// クラス
+// Interface
+// オブジェクトの型に名前をつける
+// 構造的部分型
 
-// アクセス修飾子
-// defalt: public (省略可)
-// protected, private
-
-// public
-// class User {
-//     constructor(public name: string) {
-//     }
-//     public sayHi(): void {
-//         console.log("hi! i am " + this.name);
-//     }
-// }
-
-// var tom = new User("Tom");
-// console.log(tom.name);
-// tom.sayHi();
-
-// private
-// クラス内のみで呼び出し可能
-// getter / setter を使用すれば柔軟に活用できる
-// この場合、ECMAScript5を使えと言われてerrorとなるので、コンソールでコマンド「-t ES5」を付け加える
-// class User {
-//     constructor(private _name: string) {
-//     }
-//     public sayHi(): void {
-//         console.log("hi! i am " + this._name);
-//     }
-//     // getter
-//     get name() {
-//         return this._name;
-//     }
-//     // setter
-//     set name(newValue: string) {
-//         this._name = newValue;
-//     }
-// }
-
-// var tom = new User("Tom");
-// console.log(tom.name);
-// tom.name = "TOM";
-// console.log(tom.name);
-// tom.sayHi();
-
-// クラスの継承
-// protected
-// 継承されるクラスで反映される
-// class User {
-//     constructor(protected _name: string) {
-//     }
-//     public sayHi(): void {
-//         console.log("hi! i am " + this._name);
-//     }
-// }
-
-// class AdminiUser extends User {
-//     private _age: number;
-//     constructor(_name: string, _age: number) {
-//         super(_name);
-//         this._age =_age;
-//     }
-//     public sayHi(): void {
-//         console.log("my age: " + this._age);
-//         console.log("my name: " + this._name);
-//         super.sayHi();
-//     }
-// }
-
-// var bob = new AdminiUser("Bob", 23);
-// bob.sayHi();
-
-// 静的メンバ
-class User {
-    name: string;
-    constructor(name: string) {
-        this.name = name;
-        User.count++;
-    }
-    public sayHi(): void {
-        console.log("hi! i am " + this.name);
-    }
-    static count: number = 0;
-    static showDescription(): void {
-        console.log("this class is about users");
-    }
+interface Result {
+    a: number;
+    b: number;
 }
 
-var tom = new User("Tom");
-var bob = new User("Bob");
-console.log(User.count); // 2
-User.showDescription();
+function getTotal(result: Result) {
+    return result.a + result.b;
+}
+
+var result = {
+    a: 32,
+    b: 58,
+    c: "hello" // errorにならない
+}
+
+console.log(getTotal(result)); // 90
