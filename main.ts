27de2393acf377a822027b25b5ac6dedc4a58d7c
taaako <ta12ko.sa@gmail.com>
2021-05-05 21:47:39 +0900

    dot install #14 interface inheritance

--- main.ts
+++ main.ts
@@ -2,19 +2,53 @@
 // オブジェクトの型に名前をつける
 // 構造的部分型
 
-interface Result {
+// interface Result {
+//     a: number;
+//     b: number;
+// }
+
+// function getTotal(result: Result) {
+//     return result.a + result.b;
+// }
+
+// var result = {
+//     a: 32,
+//     b: 58,
+//     c: "hello" // errorにならない
+// }
+
+// console.log(getTotal(result)); // 90
+
+
+// Interface クラスの継承
+interface SpringResult {
     a: number;
+}
+
+interface FallResult {
     b: number;
 }
 
-function getTotal(result: Result) {
-    return result.a + result.b;
+interface FinalResult extends SpringResult, FallResult {
+    final?: number;
+}
+
+// function getTotal(result: FinalResult) {
+//     return result.a + result.b + result.final;
+// }
+
+function getTotal(result: FinalResult) {
+    if (result.final) {
+        return result.a + result.b + result.final;
+    } else {
+        return result.a + result.b;
+    }
 }
 
 var result = {
     a: 32,
     b: 58,
-    c: "hello" // errorにならない
+    final: 82
 }
 
-console.log(getTotal(result)); // 90
+console.log(getTotal(result)); // 172, finalがなければ90
