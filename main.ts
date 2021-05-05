1de781655daf17d71e30095a3f032ed123638f91
taaako <ta12ko.sa@gmail.com>
2021-05-05 22:26:21 +0900

    dot install #16 generics

--- main.ts
+++ main.ts
@@ -1,76 +1,16 @@
-// Interface
-// オブジェクトの型に名前をつける
-// 構造的部分型
+// Generics
 
-// interface Result {
-//     a: number;
-//     b: number;
+// function getStringArray(value: string): string[] {
+//     return [value, value, value];
+// }
+// function getStringArray(value: number): number[] {
+//     return [value, value, value];
 // }
 
-// function getTotal(result: Result) {
-//     return result.a + result.b;
-// }
-
-// var result = {
-//     a: 32,
-//     b: 58,
-//     c: "hello" // errorにならない
-// }
-
-// console.log(getTotal(result)); // 90
-
-
-// Interface クラスの継承
-// interface SpringResult {
-//     a: number;
-// }
-
-// interface FallResult {
-//     b: number;
-// }
-
-// interface FinalResult extends SpringResult, FallResult {
-//     final?: number;
-// }
-
-// // function getTotal(result: FinalResult) {
-// //     return result.a + result.b + result.final;
-// // }
-
-// function getTotal(result: FinalResult) {
-//     if (result.final) {
-//         return result.a + result.b + result.final;
-//     } else {
-//         return result.a + result.b;
-//     }
-// }
-
-// var result = {
-//     a: 32,
-//     b: 58,
-//     final: 82
-// }
-
-// console.log(getTotal(result)); // 172, finalがなければ90
-
-
-// Interface -> Class
-
-interface GameUser {
-    score: number;
-    showScore(): void;
+// Typeを指定する
+function getArray<T>(value: T): T[] {
+    return [value, value, value];
 }
 
-class User implements GameUser {
-    name: string;
-    score: number = 0;
-    constructor(name: string) {
-        this.name = name;
-    }
-    sayHi(): void {
-        console.log("hi! i am " + this.name);
-    }
-    showScore(): void {
-        console.log("score " + this.score);
-    }
-}
+console.log(getArray<number>(3)); // [ 3, 3, 3 ]
+console.log(getArray<string>("hello")); // [ 'hello', 'hello', 'hello' ]
