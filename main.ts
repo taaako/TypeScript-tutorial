3285aa2b7f8314c14557f943644de52bd93a3854
taaako <ta12ko.sa@gmail.com>
2021-05-05 22:11:14 +0900

    dot install #15 interface class

--- main.ts
+++ main.ts
@@ -21,34 +21,56 @@
 
 
 // Interface クラスの継承
-interface SpringResult {
-    a: number;
-}
-
-interface FallResult {
-    b: number;
-}
-
-interface FinalResult extends SpringResult, FallResult {
-    final?: number;
-}
-
-// function getTotal(result: FinalResult) {
-//     return result.a + result.b + result.final;
+// interface SpringResult {
+//     a: number;
 // }
 
-function getTotal(result: FinalResult) {
-    if (result.final) {
-        return result.a + result.b + result.final;
-    } else {
-        return result.a + result.b;
+// interface FallResult {
+//     b: number;
+// }
+
+// interface FinalResult extends SpringResult, FallResult {
+//     final?: number;
+// }
+
+// // function getTotal(result: FinalResult) {
+// //     return result.a + result.b + result.final;
+// // }
+
+// function getTotal(result: FinalResult) {
+//     if (result.final) {
+//         return result.a + result.b + result.final;
+//     } else {
+//         return result.a + result.b;
+//     }
+// }
+
+// var result = {
+//     a: 32,
+//     b: 58,
+//     final: 82
+// }
+
+// console.log(getTotal(result)); // 172, finalがなければ90
+
+
+// Interface -> Class
+
+interface GameUser {
+    score: number;
+    showScore(): void;
+}
+
+class User implements GameUser {
+    name: string;
+    score: number = 0;
+    constructor(name: string) {
+        this.name = name;
+    }
+    sayHi(): void {
+        console.log("hi! i am " + this.name);
+    }
+    showScore(): void {
+        console.log("score " + this.score);
     }
 }
-
-var result = {
-    a: 32,
-    b: 58,
-    final: 82
-}
-
-console.log(getTotal(result)); // 172, finalがなければ90
